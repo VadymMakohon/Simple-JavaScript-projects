aa0f2532780d57b711c61737f0a7c644cf975e7a
VadymMakohon <errorks7@gmail.com>
2024-09-26 08:20:01 +0200

    adding a function

--- Random Quote Generator/script.js
+++ Random Quote Generator/script.js
@@ -1,5 +1,5 @@
 function generate() {
     var quotes = {
-
+        "- Deepak Chopra": '"Sex is alays about emotions. Good sex is about free emotions; bad sex is about blocked emotions."', "- C.S Lewis": '"I was not born to be free---I was born to adore and obey."', ""
     }
 }
