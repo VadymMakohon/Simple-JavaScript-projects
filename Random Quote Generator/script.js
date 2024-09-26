bd981565a72697dae7e88f591e5e3fe1bb7b5c8a
VadymMakohon <errorks7@gmail.com>
2024-09-26 08:29:44 +0200

    corrected the function and added style

--- Random Quote Generator/script.js
+++ Random Quote Generator/script.js
@@ -1,5 +1,16 @@
 function generate() {
     var quotes = {
-        "- Deepak Chopra": '"Sex is alays about emotions. Good sex is about free emotions; bad sex is about blocked emotions."', "- C.S Lewis": '"I was not born to be free---I was born to adore and obey."', ""
-    }
+        "- Deepak Chopra": '"Sex is always about emotions. Good sex is about free emotions; bad sex is about blocked emotions."',
+        "- C.S Lewis": '"I was not born to be free—I was born to adore and obey."',
+        "- Jenny Valentine": '"Even when you\'d lost everything you thought there was to lose, somebody came along and gave you something for free."'
+    };
+
+    var authors = Object.keys(quotes);
+
+    var author = authors[Math.floor(Math.random() * authors.length)];
+
+    var quote = quotes[author];
+
+    document.getElementById("quote").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
 }
