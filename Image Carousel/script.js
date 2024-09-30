d2fc73185394ad68e09fd93510653a026d617c9b
VadymMakohon <errorks7@gmail.com>
2024-09-30 12:01:17 +0200

    updated code

---
+++ Image Carousel/script.js
@@ -0,0 +1,11 @@
+var index = 0;
+
+function show_image(i) {
+    index += i;
+
+    var image = document.getElementsByClassName("image");
+
+    var dots = documnet.getElementsByClassName("dot");
+
+
+}
