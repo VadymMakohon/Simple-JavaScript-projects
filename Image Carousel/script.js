9252a4d5405643d3a6f28e9ff9a000072997b9bf
VadymMakohon <errorks7@gmail.com>
2024-09-30 12:36:28 +0200

    added function

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -1,29 +1,35 @@
 var index = 0;
 
-show_image(index);
-
-function show_image(i) {
+show_slide = (i) => {
+    //increment/decrement slide index
     index += i;
 
+    //grab all the images
     var images = document.getElementsByClassName("image");
+    //grab all the dots
     var dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < images.length; i++) {
+    // hide all the images
+    for (i = 0; i < images.length; i++)
         images[i].style.display = "none";
-    }
 
-    for (i = 0; i < dots.length; i++) {
+    // remove the active class from the dot
+    for (i = 0; i < dots.length; i++)
         dots[i].className = dots[i].className.replace(" active", "");
-    }
 
-    if (index > images.length - 1) {
+    // if index is greater than the amount of images (set it to zero)
+    if (index > images.length - 1)
         index = 0;
-    }
 
-    if (index < 0) {
+    // if index is less than zero (set it to the length of images)
+    if (index < 0)
         index = images.length - 1;
-    }
 
+    // only display the image that's next or previous
     images[index].style.display = "block";
+    // only make the current dot active
     dots[index].className += " active";
+
 }
+
+window.addEventListener("onload", show_slide(index));
