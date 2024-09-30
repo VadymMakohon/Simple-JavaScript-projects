4d05d6d69f8ee897aac6459579ee865a57bae696
VadymMakohon <errorks7@gmail.com>
2024-09-30 12:33:19 +0200

    updated code

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -1,11 +1,29 @@
 var index = 0;
 
+show_image(index);
+
 function show_image(i) {
     index += i;
 
-    var image = document.getElementsByClassName("image");
+    var images = document.getElementsByClassName("image");
+    var dots = document.getElementsByClassName("dot");
 
-    var dots = documnet.getElementsByClassName("dot");
+    for (i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
 
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 
+    if (index > images.length - 1) {
+        index = 0;
+    }
+
+    if (index < 0) {
+        index = images.length - 1;
+    }
+
+    images[index].style.display = "block";
+    dots[index].className += " active";
 }
