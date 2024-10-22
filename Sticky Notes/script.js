5567405831ee501db4fbfeb96252baa7c4a8ad41
VadymMakohon <errorks7@gmail.com>
2024-10-22 14:56:14 +0200

    added functions

--- Sticky Notes/script.js
+++ Sticky Notes/script.js
@@ -23,11 +23,13 @@ function createNote() {
 function margin() {
     var random_margin = ["-5px", "1px", "5px", "10px", "15px", "20px"];
 
-    return random_margin[Math.floor(Math.random() * random_margin.)]
+    return random_margin[Math.floor(Math.random() * random_margin.length)];
 }
 
 function rotate() {
+    var random_rotate = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-5deg)", "rotate(-10deg)"];
 
+    return random_rotate[Math.floor(Math.random() * random_rotate.length)]
 }
 
 function color() {
