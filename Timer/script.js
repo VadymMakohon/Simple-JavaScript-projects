84b9b2d1e9221ff447df734b3268b2b65edd9aba
VadymMakohon <errorks7@gmail.com>
2024-10-24 01:04:46 +0200

    typo fix

--- Timer/script.js
+++ Timer/script.js
@@ -4,7 +4,7 @@ var stoppedDuration = 0; // how long was the timer stopped?
 var startInterval = null; // this is needed to stop the startInterval()
 var flag = false; // to controll the start/stop of the timer
 
-const timerContainer = document.getElementsByClassName("timer-continer")[0];
+const timerContainer = document.getElementsByClassName("timer-container")[0];
 
 timerContainer.addEventListener("click", function () {
     if (!flag) {
@@ -12,7 +12,7 @@ timerContainer.addEventListener("click", function () {
         flag = true;
     }
     else {
-        stopTimeer();
+        stopTimer();
         flag = false;
     }
 })
@@ -21,7 +21,7 @@ function startTimer() {
     if (timeBegan === null)
         timeBegan = new Date();
 
-    if (timeStopped !== mull)
+    if (timeStopped !== null)
         stoppedDuration += (new Date() - timeStopped);
 
     startInterval = setInterval(clockRunning, 10);
@@ -29,9 +29,21 @@ function startTimer() {
 
 function stopTimer() {
     timeStopped = new Date();
-    clearInterval();
+    clearInterval(startInterval);
 }
 
 function clockRunning() {
+    var currentTime = new Date();
+    var timeElapsed = new Date(currentTime - timeBegan - stoppedDuration);
 
+    var minutes = timeElapsed.getUTCMinutes();
+    var seconds = timeElapsed.getUTCSeconds();
+    var milliseconds = timeElapsed.getUTCMilliseconds();
+
+    milliseconds = Math.floor(milliseconds / 10);
+
+    document.getElementById("timer-display").innerHTML =
+        (minutes = minutes < 10 ? '0' + minutes : minutes) + ":" +
+        (seconds = seconds < 10 ? '0' + seconds : seconds) + ":" +
+        (milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds);
 }
