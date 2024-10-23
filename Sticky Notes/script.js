1ca07cb73b649ec23533416505c044c5b51cbd40
VadymMakohon <errorks7@gmail.com>
2024-10-23 15:10:34 +0200

    updating functions

--- Sticky Notes/script.js
+++ Sticky Notes/script.js
@@ -1,42 +1,45 @@
-var container2 = document.getElementsByClassName("container2")[0];
-var container3 = document.getElementsByClassName("container3")[0];
-var checkIcon = document.getElementById("check-icon");
-var xIcon = document.getElementById("x-icon");
-var i = 0;
+var random_margin = ["-5px", "1px", "5px", "10px", "7px"];
+var random_colors = ["#c2ff3d", "#ff3de8", "#3dc2ff", "#04e022", "#bc83e6", "#ebb328"];
+var random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+var index = 0;
 
-xIcon.addEventListener("click", function () {
-    typeNote();
-})
+window.onload = document.querySelector("#user_input").select();
 
-checkIcon.addEventListener("click", function () {
-    createNote();
-})
+document.querySelector("#add_note").addEventListener("click", () => {
+    document.querySelector("#modal").style.display = "block";
+});
 
-function typeNote() {
+document.querySelector("#hide").addEventListener("click", () => {
+    document.querySelector("#modal").style.display = "none";
+});
 
-}
-
-function createNote() {
-
-}
-
-function margin() {
-    var random_margin = ["-5px", "1px", "5px", "10px", "15px", "20px"];
-
-    return random_margin[Math.floor(Math.random() * random_margin.length)];
-}
-
-function rotate() {
-    var random_rotate = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-5deg)", "rotate(-10deg)"];
-
-    return random_rotate[Math.floor(Math.random() * random_rotate.length)]
-}
-
-function color() {
-    var random_color = ["#c2ff3d", "#ff3de8", "#3dc2ff", "#04e022", "#bc83e6", "#ebb328"];
-
-    if (i > random_color.length - 1) {
-        i = 0;
+document.querySelector("#user_input").addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const text = document.querySelector("#user_input");
+        createStickyNote(text.value);
     }
-    return random_color[i++];
+});
+
+createStickyNote = (text) => {
+    let note = document.createElement("div");
+    let details = document.createElement("div");
+    let noteText = document.createElement("h1");
+
+    note.className = "note";
+    details.className = "details";
+    noteText.textContent = text;
+
+    details.appendChild(noteText);
+    note.appendChild(details);
+
+    if (index > random_colors.length - 1)
+        index = 0;
+
+    note.setAttribute("style", `margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
+
+    note.addEventListener("dblclick", () => {
+        note.remove();
+    })
+
+    document.querySelector("#all_notes").appendChild(note);
 }
