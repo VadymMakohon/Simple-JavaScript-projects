0f8dd9c482545792a01020e58582d36ad56266c5
VadymMakohon <errorks7@gmail.com>
2024-11-06 08:24:58 +0100

    updating js

--- Quotes API Typewriter/script.js
+++ Quotes API Typewriter/script.js
@@ -3,3 +3,13 @@ var index = 0;
 var textPosition = 0;
 var flag = true;
 var destination = document.getElementById("typedtext");
+
+function loadQuote() {
+    const url = "https://api.quotable.io/random";
+
+    fetch(url)
+
+        .then(response => {
+
+        })
+}
