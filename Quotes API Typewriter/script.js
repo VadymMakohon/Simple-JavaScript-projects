b2a375ca898c5e8ad0ec2963f20ffcec1c2e93eb
VadymMakohon <errorks7@gmail.com>
2024-11-07 13:02:45 +0100

    updating js

--- Quotes API Typewriter/script.js
+++ Quotes API Typewriter/script.js
@@ -17,6 +17,26 @@ function loadQuote() {
         })
 
         .then(data => {
-
+            quoteArray[index] = data.content;
         })
+}
+
+function typewriter() {
+    if (flag) {
+        loadQuote();
+        quoteArray[index] += '';
+        flag = false;
+    }
+
+    destination.innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE<span>';
+
+    if (textPosition++ != quoteArray[index].length) {
+        setTimeout('typewriter()', 100);
+    }
+    else {
+        quoteArray[index] = '';
+        setTimeout('typewriter()', 3000);
+        textPosition = 0;
+        flag = true;
+    }
 }
