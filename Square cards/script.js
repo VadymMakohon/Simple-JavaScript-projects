210a81822b11291b7c1e37ebaf87d8117bbba32b
VadymMakohon <errorks7@gmail.com>
2024-11-09 21:02:21 +0100

    updated html

--- Square cards/script.js
+++ Square cards/script.js
@@ -1,23 +1,23 @@
-document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", () => {
-    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+document.getElementsByClassName("article")[0].addEventListener("click", () => {
+    window.open("https://vadymmakohon.github.io/Groceries-List/");
 });
 
 document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
     window.open("https://www.youtube.com/watch?v=wtqbOFYivxw");
 });
 
-document.getElementsByClassName("article")[0].addEventListener("click", () => {
+document.getElementsByClassName("article")[2].addEventListener("click", () => {
     window.open("https://vadymmakohon.github.io/to-do-list-app/");
 });
 
-document.getElementsByClassName("article")[1].addEventListener("click", () => {
+document.getElementsByClassName("article")[3].addEventListener("click", () => {
     window.open("https://vadymmakohon.github.io/Weather-App/");
 });
 
-document.getElementsByClassName("article")[2].addEventListener("click", () => {
+document.getElementsByClassName("article")[4].addEventListener("click", () => {
     window.open("https://iamcodefoxx.github.io/Quiz-App/");
 });
 
-document.getElementsByClassName("article")[3].addEventListener("click", () => {
+document.getElementsByClassName("article")[5].addEventListener("click", () => {
     window.open("https://iamcodefoxx.github.io/Marvel-API/");
 });
