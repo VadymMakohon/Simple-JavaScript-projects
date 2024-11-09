ee87a76ad117a7802aebe17169497532be8434e3
VadymMakohon <errorks7@gmail.com>
2024-11-09 21:17:23 +0100

    updated script

--- Square cards/script.js
+++ Square cards/script.js
@@ -1,23 +1,25 @@
-document.getElementsByClassName("article")[0].addEventListener("click", () => {
-    window.open("https://vadymmakohon.github.io/Groceries-List/");
-});
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByClassName("article")[0].addEventListener("click", () => {
+        window.open("https://vadymmakohon.github.io/Groceries-List/");
+    });
 
-document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
-    window.open("https://www.youtube.com/watch?v=wtqbOFYivxw");
-});
+    document.getElementsByClassName("")[1].addEventListener("click", () => {
+        window.open("");
+    });
 
-document.getElementsByClassName("article")[2].addEventListener("click", () => {
-    window.open("https://vadymmakohon.github.io/to-do-list-app/");
-});
+    document.getElementsByClassName("article")[2].addEventListener("click", () => {
+        window.open("https://vadymmakohon.github.io/to-do-list-app/");
+    });
 
-document.getElementsByClassName("article")[3].addEventListener("click", () => {
-    window.open("https://vadymmakohon.github.io/Weather-App/");
-});
+    document.getElementsByClassName("article")[4].addEventListener("click", () => {
+        window.open("https://vadymmakohon.github.io/Weather-App/");
+    });
 
-document.getElementsByClassName("article")[4].addEventListener("click", () => {
-    window.open("https://iamcodefoxx.github.io/Quiz-App/");
-});
+    document.getElementsByClassName("article")[5].addEventListener("click", () => {
+        window.open("https://iamcodefoxx.github.io/Quiz-App/");
+    });
 
-document.getElementsByClassName("article")[5].addEventListener("click", () => {
-    window.open("https://iamcodefoxx.github.io/Marvel-API/");
+    document.getElementsByClassName("article")[6].addEventListener("click", () => {
+        window.open("https://iamcodefoxx.github.io/Marvel-API/");
+    });
 });
