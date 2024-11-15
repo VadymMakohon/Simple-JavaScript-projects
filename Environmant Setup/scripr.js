f7764904b9f5f9bfd2fc0ee666dedbc427c804cc
VadymMakohon <errorks7@gmail.com>
2024-11-15 12:27:30 +0100

    updating script

--- Environmant Setup/scripr.js
+++ Environmant Setup/scripr.js
@@ -1,10 +1,10 @@
-const apikey = 'f22a10ed8a5a41c1b168829fa6954a65'
+const apiKey = "f22a10ed8a5a41c1b168829fa6954a65"
 
 const blogContainer = document.getElementById("blog-container");
 
 async function fetchRandomNews() {
     try {
-        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
@@ -16,25 +16,33 @@ async function fetchRandomNews() {
 
 function displayBlogs(articles) {
     blogContainer.innerHTML = ""
-    articles.forEach((articles) => {
-        const blogcard = document.createElement
-            ("div")
-        blogcard.classList.add("blog-card")
-        const img = documents.createElement("img")
+    articles.forEach((article) => {
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+
+        const img = document.createElement("img"); // Fixed typo here
         img.src = article.urlToImage
         img.alt = article.title
-        const title = document.createElement("h2")
+
+        const title = document.createElement("h2");
         title.textContent = article.title
-        const description = document.createElement("p")
+
+        const description = document.createElement("p");
         description.textContent = article.description
-    })
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+
+        blogContainer.appendChild(blogCard);
+    });
 }
 
-async () => {
+(async () => {
     try {
         const articles = await fetchRandomNews();
         displayBlogs(articles);
     } catch (error) {
         console.error("Error fetching random news", error);
     }
-};
+})();
