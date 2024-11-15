94e85bf95a893e2b6d800de94ed0afeba592e17d
VadymMakohon <errorks7@gmail.com>
2024-11-15 12:07:57 +0100

    adding script

--- Environmant Setup/scripr.js
+++ Environmant Setup/scripr.js
@@ -5,10 +5,27 @@ const blogContainer = document.getElementById("blog-container");
 async function fetchRandomNews() {
     try {
         const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`
-        const response = await fetch(apiUrl)
-
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
     } catch (error) {
-        console.error("Error fetching random news", error)
+        console.error("Error fetching random news", error);
         return []
     }
 }
+
+function displayBlogs(articles) {
+    blogContainer.innerHTML = ""
+    articles.forEach((articles) => {
+
+    })
+}
+
+async () => {
+    try {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
+    }
+};
